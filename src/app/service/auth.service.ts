949ae041ec59595558c0a50b828d7e3e66dd9ddf
Gisiqueira96 <giovanna.penedos@outlook.com>
2021-09-30 16:48:21 -0300

    Componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -14,12 +15,22 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  entrar (userLogin:UserLogin):Observable<UserLogin>{
-    return this.http.post<UserLogin>('https://backendblogpessoal.herokuapp.com/usuarios/logar', userLogin)
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('https://blogpessoaldagi.herokuapp.com/usuarios/logar', userLogin)
   }
 
-  cadastrar (user: User): Observable<User>{
-    return this.http.post<User>('https://backendblogpessoal.herokuapp.com/usuarios/cadastrar', user)
+  cadastrar(user: User): Observable<User> {
+    return this.http.post<User>('https://blogpessoaldagi.herokuapp.com/usuarios/cadastrar', user)
 
+  }
+
+  logado() {
+    let ok: boolean = false
+
+    if (environment.token != '') {
+      ok = true
+    }
+
+    return ok
   }
 }
