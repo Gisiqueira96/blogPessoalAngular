7094195f5ffeb67427b81d4596358188e1b18889
Gisiqueira96 <giovanna.penedos@outlook.com>
2021-10-06 22:38:51 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -21,7 +21,10 @@ export class AuthService {
 
   cadastrar(user: User): Observable<User> {
     return this.http.post<User>('https://blogpessoaldagi.herokuapp.com/usuarios/cadastrar', user)
+  }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogpessoaldagi.herokuapp.com/usuarios/${id}`)
   }
 
   logado() {
